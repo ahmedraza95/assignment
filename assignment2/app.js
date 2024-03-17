5ae433dd817023225b5656fdac9d7ef101845956
ahmedraza95 <ahmedhome6789@gmail.com>
2024-03-18 02:10:30 +0500

    first

---
+++ assignment2/app.js
@@ -0,0 +1,26 @@
+let userName;
+let myName = "Ahmed Raza";
+
+let message = "Hello World";
+alert(message);
+
+
+let username = "John";
+let age = "19";
+let skills = "certified mobile application development"
+
+alert(username);
+alert(age);
+alert(skills);
+
+
+alert("pizza\npizz\npiz\npi\np")
+
+let email = "example@example.com";
+alert("my email address is " + email)
+
+let book = "A smarter way to learn javaScript";
+alert("i am trying to learn from the book " + book);
+
+let design = " “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” ";
+alert(design)
